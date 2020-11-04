d373b73385635a75c06f6ab4bd2dc6e0ab4aed26
YukimiSnow <73086228+YukimiSnow@users.noreply.github.com>
2020-11-04 17:54:47 +0800

    Yes
    Yes

---
+++ MiniTest2/Assets/Script/CubeMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+
+    float speed = 20.0f;
+    float zLimit = 20.0f;
+    float RezLimit = -1.0f;
+
+    bool PlusLimit = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < zLimit && PlusLimit == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+        else if (transform.position.z > RezLimit && PlusLimit == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+        }
+        else
+        {
+            PlusLimit = !PlusLimit; //Changing PlusLimit into false
+        }
+
+    }
+}
---
+++ MiniTest2/Assets/Script/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declaration
+
+    float gravityModifer = 2.0f;
+    float speed = 10.0f;
+    bool OnMovingCube;
+    Rigidbody playerRb;
+
+    public GameObject MovingCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifer;
+
+        playerRb = GetComponent<Rigidbody>(); //Make use of "Rigidbody"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Declare and Init variables to erference to User Interaction inputs
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Jump();
+
+        //Move Player (GameOject) according to user interactions
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (OnMovingCube == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, MovingCube.transform.position.z);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Moving Cube"))
+        {
+            OnMovingCube = true;
+        }
+        if (collision.gameObject.CompareTag("StartingPoint"))
+        {
+            OnMovingCube = false;
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+            OnMovingCube = false;
+        }
+    }
+}
